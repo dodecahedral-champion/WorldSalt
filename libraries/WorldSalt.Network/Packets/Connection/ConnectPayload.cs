78d87b07021fc61361d40ec73b9ad69f03ed6651
Dodecahedral Champion <dodecahedral_champion@yahoo.co.uk>
2015-11-29 14:10:58 +0000

    use stream-based deserialisation to make usage a bit easier

--- libraries/WorldSalt.Network/Packets/Connection/ConnectPayload.cs
+++ libraries/WorldSalt.Network/Packets/Connection/ConnectPayload.cs
@@ -1,6 +1,7 @@
 namespace WorldSalt.Network.Packets.Connection {
 	using System;
 	using System.Collections.Generic;
+	using System.IO;
 	using System.Linq;
 	using WorldSalt.Network.Direction;
 	using WorldSalt.Network.Serialisation;
@@ -33,11 +34,11 @@ namespace WorldSalt.Network.Packets.Connection {
 			UInt64 preferredProtocol;
 			IList<UInt64> supportedProtocols;
 
-			UInt64 index = 0;
-			index = SerialisationHelper.LoadString(bytes, index, out username);
-			index = SerialisationHelper.LoadU64(bytes, index, out preferredProtocol);
-			index = SerialisationHelper.LoadList(bytes, index, out supportedProtocols, SerialisationHelper.LoadU64);
-			SerialisationHelper.AssertEnd(bytes, index);
+			new MemoryStream(bytes)
+				.Deserialise(out username)
+				.Deserialise(out preferredProtocol)
+				.Deserialise(out supportedProtocols, SerialisationHelper.Deserialise)
+				.AssertEnd();
 
 			Username = username;
 			PreferredProtocol = preferredProtocol;
--- libraries/WorldSalt.Network/Serialisation/SerialisationHelper.cs
+++ libraries/WorldSalt.Network/Serialisation/SerialisationHelper.cs
@@ -1,12 +1,12 @@
 namespace WorldSalt.Network.Serialisation {
 	using System;
 	using System.Collections.Generic;
+	using System.IO;
 	using System.Linq;
 	using System.Text;
-	using Index = System.UInt64;
 
 	public static class SerialisationHelper {
-		public delegate Index Loader<T>(Byte[] bytes, Index startIndex, out T value);
+		public delegate Stream Deserialiser<T>(Stream stream, out T value);
 		public delegate IEnumerable<Byte[]> Serialiser<T>(T self);
 
 		public static IEnumerable<Byte[]> Serialise(this string self) {
@@ -33,29 +33,29 @@ namespace WorldSalt.Network.Serialisation {
 			return length.Serialise().Concat(fragments.SelectMany(x => x));
 		}
 
-		public static Index LoadString(Byte[] bytes, Index startIndex, out string value) {
+		public static Stream Deserialise(this Stream stream, out string value) {
 			UInt64 length;
-			var index = LoadU64(bytes, startIndex, out length);
-			Byte[] stringBytes;
-			index = ReadByteSequence(bytes, index, out stringBytes, length);
-			value = Encoding.UTF8.GetString(stringBytes);
-			return index;
+			stream = stream.Deserialise(out length);
+			value = Encoding.UTF8.GetString(stream.ReadByteSequence(length));
+			return stream;
 		}
 
-		public static Index LoadU64(Byte[] bytes, Index startIndex, out UInt64 value) {
-			return ReadUnsignedInteger(bytes, startIndex, out value, 8);
+		public static Stream Deserialise(this Stream stream, out UInt64 value) {
+			return stream.DeserialiseUnsignedInteger(out value, 8);
 		}
 
-		public static Index LoadList<TItem>(Byte[] bytes, Index startIndex, out IList<TItem> value, Loader<TItem> itemLoader) {
+		public static Stream Deserialise<TItem>(this Stream stream, out IList<TItem> value, Deserialiser<TItem> deserialiser) {
 			UInt64 length;
-			var index = LoadU64(bytes, startIndex, out length);
-			return ReadSequence(bytes, index, out value, length, itemLoader);
+			stream = stream.Deserialise(out length);
+			value = stream.ReadSequence<TItem>(length, deserialiser).ToList();
+			return stream;
 		}
 
-		public static void AssertEnd(Byte[] bytes, Index index) {
-			var bytesRemaining = (UInt64)bytes.Length - index;
-			if(bytesRemaining != 0) {
-				throw new FormatException(string.Format("expected end of buffer: bytes remaining = {0}", bytesRemaining));
+		public static void AssertEnd(this Stream stream) {
+			var oldPosition = stream.Position;
+			var endPosition = stream.Seek(0, SeekOrigin.End);
+			if(endPosition != oldPosition) {
+				throw new FormatException("expected end of buffer");
 			}
 		}
 
@@ -72,43 +72,29 @@ namespace WorldSalt.Network.Serialisation {
 			return combinedBuffer;
 		}
 
-		private static Index ReadByteSequence(Byte[] bytes, Index startIndex, out Byte[] sequence, UInt64 numBytes) {
-			var desiredMinLength = startIndex + numBytes;
-			if(desiredMinLength > (UInt64)bytes.LongLength) {
-				throw new FormatException(String.Format("cannot read sequence of {0} bytes; expected at least {1} bytes in buffer but found only {2}", numBytes, desiredMinLength, bytes.LongLength));
+		private static Byte[] ReadByteSequence(this Stream stream, UInt64 numBytes) {
+			var smallLength = (int)numBytes;
+			var sequence = new Byte[smallLength];
+			var numRead = stream.Read(sequence, 0, smallLength);
+			if(numRead != smallLength) {
+				throw new FormatException(String.Format("tried to read sequence of {0} bytes, but only found {1}", smallLength, numRead));
 			}
 
-			var working = new byte[numBytes];
-			for (UInt64 i = 0; i < numBytes; i++) {
-				working[i] = bytes[i + startIndex];
-			}
-
-			sequence = working;
-			return startIndex + numBytes;
+			return sequence;
 		}
 
-		private static Index ReadSequence<TItem>(Byte[] bytes, Index startIndex, out IList<TItem> sequence, UInt64 numItems, Loader<TItem> itemLoader) {
-			var working = new List<TItem>((int)numItems);
-			var index = startIndex;
-			TItem item;
-			for (UInt64 i = 0; i < numItems; i++) {
-				index = itemLoader(bytes, index, out item);
-				working.Add(item);
+		private static IEnumerable<TItem> ReadSequence<TItem>(this Stream stream, UInt64 numItems, Deserialiser<TItem> deserialiser) {
+			foreach (var i in Enumerable.Repeat(0, (int)numItems)) {
+				TItem item;
+				stream = deserialiser(stream, out item);
+				yield return item;
 			}
-
-			sequence = working;
-			return index;
 		}
 
-		private static Index ReadUnsignedInteger(Byte[] bytes, Index startIndex, out UInt64 value, UInt16 integerSizeBytes) {
-			Byte[] sequence;
-			var index = ReadByteSequence(bytes, startIndex, out sequence, integerSizeBytes);
-			if(sequence.Length != integerSizeBytes) {
-				throw new FormatException(string.Format("could not read u{1} starting at {0}", startIndex, integerSizeBytes*8));
-			}
-
-			value = sequence.Reverse().Aggregate((UInt64)0, (working, b) => working * 256 + b);
-			return index;
+		private static Stream DeserialiseUnsignedInteger(this Stream stream, out UInt64 value, UInt16 integerSizeBytes) {
+			value = stream.ReadByteSequence(integerSizeBytes).Reverse()
+				.Aggregate((UInt64)0, (working, b) => working * 256 + b);
+			return stream;
 		}
 
 		private static IEnumerable<Byte> SerialiseUnsignedInteger(UInt64 value, UInt16 integerSizeBytes) {
