d9be2aaa452183c03571d92aa5556ab73d7c780f
Dodecahedral Champion <dodecahedral_champion@yahoo.co.uk>
2015-11-30 01:37:43 +0000

    unit tests for UntypedFrame

--- libraries/WorldSalt.UnitTests.Network/Frames/UntypedFrameUnitTest.cs
+++ libraries/WorldSalt.UnitTests.Network/Frames/UntypedFrameUnitTest.cs
@@ -1,12 +1,33 @@
 namespace WorldSalt.UnitTests.Network.Frames {
 	using System;
-	using System.Collections.Generic;
 	using NUnit.Framework;
 	using Rhino.Mocks;
 	using WorldSalt.Network.Direction;
 	using WorldSalt.Network.Frames;
+	using WorldSalt.Network.Payloads;
 
 	[TestFixture]
 	public class UntypedFrameUnitTest {
+		[Test]
+		public void ShouldConstructWithDefaults() {
+			var target = new UntypedFrame<FromClient>();
+
+			Assert.AreEqual(0x00, target.Type);
+			Assert.AreEqual(0x00, target.Subtype);
+			Assert.NotNull(target.Payload);
+			Assert.AreEqual(0, target.Payload.Length);
+		}
+
+		[Test]
+		public void ShouldConstructWithArguments() {
+			var expectedType = (byte)0x12;
+			var expectedSubtype = (byte)0x34;
+			var expectedPayload = MockRepository.GenerateMock<IRawPayload<FromClient>>();
+			var target = new UntypedFrame<FromClient>(expectedType, expectedSubtype, expectedPayload);
+
+			Assert.AreEqual(expectedType, target.Type);
+			Assert.AreEqual(expectedSubtype, target.Subtype);
+			Assert.AreSame(expectedPayload, target.Payload);
+		}
 	}
 }
